7f7e8cb19331b44014f5fab0c5c0c5a9e3fc6cdc
sujit <sujittech06@gmail.com>
2024-05-17 22:53:23 +0530

    dockerfile added

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM tomcat:8.0.20-jre8
+EXPOSE 8080 8443
+COPY target/dockerProject.war.war /usr/local/tomcat/webapps/dockerProject.war.war
+CMD ["catalina.sh", "run"]
--- src/main/java/com/example/dockers/com/DockerProjectApplication.java
+++ src/main/java/com/example/dockers/com/DockerProjectApplication.java
@@ -2,12 +2,19 @@ package com.example.dockers.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DockerProjectApplication {
+public class DockerProjectApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DockerProjectApplication.class, args);
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	
+		return builder.sources(DockerProjectApplication.class);
 	}
 
 }
